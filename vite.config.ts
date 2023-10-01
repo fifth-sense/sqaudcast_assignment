f61cd355d1490ce9c0cf5f5be671a88556a3ec0d
Ruby <rubychoudhary1711@gmail.com>
2023-10-01 17:21:18 +0530

    config

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./sqaudcast_assignment",
+  base: "./",
   plugins: [react()],
   test:{
     globals: true,
